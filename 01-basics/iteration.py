269003e458f6434246f729784cbb92a5e039654a
saiKelkar <104817425+saiKelkar@users.noreply.github.com>
2026-03-05 11:26:02 +0530

    Create iteration.py

---
+++ 01-basics/iteration.py
@@ -0,0 +1,13 @@
+# while statement
+n = 5
+while n > 0:
+    print(n, end=' ')
+    n = n - 1
+print('Blastoff!')
+
+# for loops
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends:
+    print(f'Happy new year, {friend}!')
+print('Done')
+
